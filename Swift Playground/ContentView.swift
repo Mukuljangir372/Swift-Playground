9fcbe3dc377edbacab84cef0dc4947121996b819
mukul372 <mukul@scogo.in>
2022-05-31 10:55:59 +0530

    Initial Commit

---
+++ Swift Playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Playground
+//
+//  Created by Mukul Jangir on 31/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Playground/Swift_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_PlaygroundApp.swift
+//  Swift Playground
+//
+//  Created by Mukul Jangir on 31/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
